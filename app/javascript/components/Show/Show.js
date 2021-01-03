362232b22bf1734f64fed8011117357a01a1a459
sarafiskray <sarafiskray@gmail.com>
2021-01-03 17:41:08 -0500

    added two column CSS

--- app/javascript/components/Show/Show.js
+++ app/javascript/components/Show/Show.js
@@ -1,6 +1,34 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Header from './Header'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+`
+const Column = styled.div`
+    height: 100vh;
+    overflow: scroll;
+    scrollbar-width: none; 
+    -ms-overflow-style: none; 
+
+    &:last-child {
+        background: grey;
+    }
+
+    ::-webkit-scrollbar { 
+        width: 0;
+        height: 0;
+    }
+`
+
+const HeaderWrapper = styled.div`
+    padding-left: 50px;
+`
+
 
 const Show = (props) => {
 
@@ -22,24 +50,26 @@ const Show = (props) => {
     }, [])
 
     return(
-        <div>
-            <div className="column">
-                {loaded &&
-                <Header
-                    attributes={show.data.attributes}
-                    reviews= {show.included}
-                />
-                }
-                <div className="reviews">  
-                    [Reviews Will Go Here]
-                </div>
-            </div>
-            <div className="column">
+        <Wrapper>
+            <Column>
+                <HeaderWrapper>
+                    {loaded &&
+                    <Header
+                        attributes={show.data.attributes}
+                        reviews= {show.included}
+                    />
+                    }
+                    <div className="reviews">  
+                        [Reviews Will Go Here]
+                    </div>`
+                </HeaderWrapper>
+            </Column>
+            <Column>
                 <div className="review-form">
                     [Review Form Will Go Here]
                 </div>
-            </div>
-        </div>
+            </Column>
+        </Wrapper>
     )
 }
 
