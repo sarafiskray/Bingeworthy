03ec709b2d7a94b6c0304bb4fbdb9996665e4b52
sarafiskray <sarafiskray@gmail.com>
2021-01-16 00:48:26 -0500

    moved user pool data to userpool.js, and added userpool.js to gitignore

--- app/javascript/components/Cognito/SignUp.js
+++ app/javascript/components/Cognito/SignUp.js
@@ -1,19 +1,11 @@
 import React, { useState } from 'react'
-import { CognitoUserPool } from 'amazon-cognito-identity-js'
+import UserPool from './UserPool'
 
 const SignUp = () => {
 
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-
-
-    const poolData = {
-        UserPoolId: 'us-east-2_Da69i358x',
-        ClientId: '4vtp2186e00ibl8ujari51scl2'
-    }
-
-    const userPool = new CognitoUserPool(poolData)
 
 
     const onSubmit = event => {
@@ -23,7 +15,7 @@ const SignUp = () => {
             { Name: 'email', Value: email}
         ]
 
-        userPool.signUp(username, password, attributes, null, (err, data) => {
+        UserPool.signUp(username, password, attributes, null, (err, data) => {
             if (err) console.log(err)
             console.log(data)
         })
